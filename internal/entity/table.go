f84830defdf6d1629b45089e06fa7f906084ef24
liangxiangtao <liangliang.too@foxmail.com>
2025-04-14 17:38:54 +0800

    db entity

---
+++ internal/entity/table.go
@@ -0,0 +1,11 @@
+package entity
+
+const (
+	ClassTable   = "class"   // 班级表
+	ExamTable    = "exam"    // 考试表
+	GradeTable   = "grade"   // 学生成绩表
+	RoleTable    = "role"    // 角色表
+	StudentTable = "student" // 学生表
+	SubjectTable = "subject" // 学科表
+	TeacherTable = "teacher" // 教师表
+)
